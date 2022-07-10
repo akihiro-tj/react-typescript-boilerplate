c2660775efe01a75e2a41f0fb3728017ac14b634
Akihiro Tojo <akihiro.02.tj@gmail.com>
2022-07-10 21:49:58 +0900

    Lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import { Counter } from './features/counter/Counter';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
