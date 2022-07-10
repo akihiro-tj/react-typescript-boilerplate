5aa24efdd44c745618408d1d99d07e5f77aab89e
Akihiro Tojo <akihiro.02.tj@gmail.com>
2022-07-10 21:49:21 +0900

    Update packages

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const path = require('path');
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
+);
+
+module.exports = {
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'prettier/prettier': ['error', prettierOptions],
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+        'newlines-between': 'always',
+        pathGroupsExcludedImportTypes: ['builtin'],
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        pathGroups: [],
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      rules: { 'prettier/prettier': ['warn', prettierOptions] },
+    },
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
